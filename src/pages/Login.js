e194ff4ac63d006a2b17aa2c545f22b14be4dc2d
Exequiel65 <marcos97britos@gmail.com>
2022-04-22 21:22:23 -0300

    add login v1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../auth/AuthContext';
+import { types } from '../types';
 
 export const Login = () => {
+  
+  const {dispatch} = useContext(AuthContext);
   const handleLogin = () =>{
-    
+    dispatch({
+      type : types.login,
+      payload : {
+        name : "Marcos"
+      }
+    })
   }
   return (
     <div className='mt-5'>
